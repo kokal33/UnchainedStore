185e5abd535e92da19e4819054b6321978d7f953
Oliver Vasilev <oliver_vasilev@outlook.com>
2021-03-30 16:02:49 +0200

    design updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UnchainedStore';
 
-  navLinks = [
-    {location:'',label:'dummy',icon:'menu'},
-    { location: '/shared', label: 'Overview', icon: 'account_circle' },
-    { location: '/shared/sub', label: 'Experience', icon: 'work' }
-  ];
+
 }
