d407832c2ccb94d6905e8c55b1ede02d410f7ff5
Oliver Vasilev <oliver_vasilev@outlook.com>
2021-04-22 19:15:23 +0200

    nft listings changes and added modal details

--- src/app/music/components/card-view-list/card-view.component.ts
+++ src/app/music/components/card-view-list/card-view.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Track } from 'ngx-audio-player';
+import { DialogService } from 'primeng/dynamicdialog';
 import { ProductService } from 'src/app/services/productService';
+import { CardViewDetailsComponent } from '../card-view-details/card-view-details.component';
 
 @Component({
   selector: 'app-card-view',
   templateUrl: './card-view.component.html',
   styleUrls: ['./card-view.component.scss'],
-  providers: [ProductService]
+  providers: [ProductService, DialogService]
 })
 export class CardViewComponent implements OnInit {
 
 
-  fileSource = "https://rildi.sunproxy.net/file/Z1Z4cEpyWi95VWtISXU2NzJqMW9meGlRZWpsZExFcTJxMFhERUFnRFZseFlEVTVhcENGRWVmTkIxeEV3Wk55RE13aUR4cDZNaWttOVlqWUFUOXVvaU45ZHo3ckUyRXFlbDE2cWxjcCtiQ3c9/Alanis_Morissette_-_Not_As_We_(Hydr0.org).mp3";
+  fileSource = "https://rildi.sunproxy.net/file/MGpUNWxKVjVCVWE2SDB0My9EU3hlWDdBRVUrQkM0c2pqRHBVbHNZd01FRDRSajRXbmNUTDRpSitTcC91QU40MmtES1g2eUpsOEd6bzJqRnE2cm5KN0NTVnhPcTNoK3RrdGgxMnpLQ09TYkk9/The_Weeknd_-_Save_Your_Tears_(Rilds.com).mp3";
 
-  constructor() { }
-
-
-
-
+  constructor(private dialogService: DialogService) { }
   // AUDIO PLAYER CONTROLS
   msaapDisplayTitle = false;
   msaapDisplayPlayList = false;
@@ -39,8 +37,17 @@ export class CardViewComponent implements OnInit {
   ];
 
 
+
   ngOnInit() {
 
+  }
+
+  viewDetails() {
+    const dialog = this.dialogService.open(CardViewDetailsComponent, {
+      width: "70%",
+      header: '',
+      data: {}
+    });
   }
 
   onEnded(event: any) {
