71e2004c71fab800f921b3a10012d0ac10f42931
kokal33 <vasilev.s@protonmail.com>
2021-05-30 01:01:01 +0200

    changed backend url without slash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://store.unchained-music.com/',
+  apiUrl: 'https://store.unchained-music.com',
   production: true,
   contractTestAddress: "0x894c75da281e9ea07c17d270aaa703242e3298ba"
 };
