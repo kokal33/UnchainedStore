68e476976fb2083c671e31fbe4b8b31bc95276d5
Oliver Vasilev <oliverv@nextsense.com>
2021-05-30 03:55:17 +0200

    marketplace grid

--- src/app/01.Marketplace/components/marketplace-grid/marketplace-grid.component.ts
+++ src/app/01.Marketplace/components/marketplace-grid/marketplace-grid.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Track } from 'ngx-audio-player';
 import { DialogService } from 'primeng/dynamicdialog';
 import { BackendService } from 'src/app/07.Services/backendService';
+import { environment } from 'src/environments/environment';
 import { MarketplaceItemDialogComponent } from '../marketplace-item-dialog/marketplace-item-dialog.component';
 
 @Component({
   selector: 'app-marketplace-grid',
   templateUrl: './marketplace-grid.component.html',
   styleUrls: ['./marketplace-grid.component.scss'],
-  providers: [DialogService,BackendService]
+  providers: [DialogService,BackendService],
 })
 export class MarketplaceGridComponent implements OnInit {
 
-  fileSource = "https://filesamples.com/samples/audio/mp3/sample3.mp3";
   marketplaceItems: any[] = [];
-  constructor(private dialogService: DialogService, private backendService : BackendService) { }
+  environment! : any;
+  constructor(private dialogService: DialogService, private backendService : BackendService,private changeDetectorRef: ChangeDetectorRef) { }
   msaapDisplayTitle = false;
   msaapDisplayPlayList = false;
   msaapPageSizeOptions = [2, 4, 6];
@@ -24,25 +25,17 @@ export class MarketplaceGridComponent implements OnInit {
   msaapDisplayDuration = false;
   msaapDisablePositionSlider = true;
 
-  // Material Style Advance Audio Player Playlist
-  msaapPlaylist: Track[] = [
-    {
-      title: 'Audio One Title',
-      link: this.fileSource,
-      artist: 'Audio One Artist',
-      duration: 50
-    }
-  ];
+
 
   async ngOnInit() {
+    this.environment = environment.apiUrl;
     this.marketplaceItems =  (await this.backendService.getTracks()).body;
   }
   viewDetails() {
     const dialog = this.dialogService.open(MarketplaceItemDialogComponent, {
-      header: '@Oliver',
+      header: '',
       data: {},
     });
-
   }
 
   onEnded(event: any) {
