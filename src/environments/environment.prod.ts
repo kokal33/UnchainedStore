8ed98a80c3978d540c6dc46eb744a318e86736dd
kokal33 <vasilev.s@protonmail.com>
2021-07-04 12:36:26 +0200

    changed unchained token, buttons visibility

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiUrl: 'https://store.unchained-music.com',
   production: true,
-  contractTestAddress: "0xd2eb9798925D9Bd78EeF77a61011f1925DD54cAB",
+  contractTestAddress: "0x23c56911f19074c3a915CaC538d4f7d0bc16a36B",
   marketplaceTestAddress: "0xce70d983f3911f0d1fea6b06a19f2DE6650f371f"
 };
