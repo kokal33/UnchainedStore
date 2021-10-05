5369fff40c5608cdc9d5e71ae69e6f6e00214528
kokal33 <vasilev.s@protonmail.com>
2021-10-05 16:55:35 +0200

    removed biconomy from app

--- src/app/07.Services/web3Service.ts
+++ src/app/07.Services/web3Service.ts
@@ -1,25 +1,6 @@
 import Web3 from 'web3';
-import Biconomy from '@biconomy/mexa';
+export function getWeb3(): Web3 {
 
-export function getWeb3() {
-  const apiKey = '8UJW18alH.6d6126e2-2fcb-4689-8354-194057a047aa';
-  const biconomy = new Biconomy(window.ethereum,{apiKey});
-  return new Web3(biconomy);
+    const ethereum = window.ethereum as any;
+    return new Web3(ethereum);
   }
-
-export function getBiconomy() {
-  const apiKey = '8UJW18alH.6d6126e2-2fcb-4689-8354-194057a047aa';
-  return new Biconomy(window.ethereum,{apiKey});
-};
-
-
-// OLD VERSION OF WEB3
-// export const getWeb3 = async () => {
-//   let web3: Web3;
-
-//   // modern dApp browsers
-//   if (window.ethereum) {
-//     const ethereum = window.ethereum as any;
-//     web3 = new Web3(ethereum);
-//   }
-//   }
